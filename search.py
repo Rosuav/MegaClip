f2455a3dc5a7a47d17f0fbf19341e91131d32493
Chris Angelico <rosuav@gmail.com>
2018-03-25 18:25:47 +1100

    search: Turn the body into a function and parameterize it

--- search.py
+++ search.py
@@ -1,21 +1,26 @@
 import sys
 import megaclip
 
-video = "242164831"
-search_terms = ["kayosg", "rvcatu"]
+def search(video, *search_terms):
+	info = megaclip.get_video_info(video, verbose=True)
 
-info = megaclip.get_video_info(video, verbose=True)
+	for msg in info["comments"]:
+		for findme in search_terms:
+			if findme in msg["message"]["body"]: break
+		else:
+			continue # Not found? Don't display it.
+		secs = int(msg["content_offset_seconds"])
+		tm = "[%d:%02d:%02d]" % (secs // 3600, (secs // 60) % 60, secs % 60)
+		name = msg["commenter"]["display_name"]
+		# TODO (maybe): Colorize the name
+		if msg["message"]["is_action"]:
+			print(tm, name, msg["message"]["body"])
+		else:
+			print(tm, name + ":", msg["message"]["body"])
 
-for msg in info["comments"]:
-	for findme in search_terms:
-		if findme in msg["message"]["body"]: break
+if __name__ == "__main__":
+	if len(sys.argv) < 3:
+		print("USAGE: python3 %s videoid search_term [search_term...]" % sys.argv[0])
+		print("Searches that video's chat for any of the given words")
 	else:
-		continue # Not found? Don't display it.
-	secs = int(msg["content_offset_seconds"])
-	tm = "[%d:%02d:%02d]" % (secs // 3600, (secs // 60) % 60, secs % 60)
-	name = msg["commenter"]["display_name"]
-	# TODO (maybe): Colorize the name
-	if msg["message"]["is_action"]:
-		print(tm, name, msg["message"]["body"])
-	else:
-		print(tm, name + ":", msg["message"]["body"])
+		search(*sys.argv[1:])
