c47d0ffee0916cb782523a2283b7ecc61b0a4cf4
Chris Angelico <rosuav@gmail.com>
2018-05-27 01:36:54 +1000

    Allow prepopulation of the cache for rave searching

--- rave.py
+++ rave.py
@@ -2,7 +2,11 @@ import sys
 import os
 import megaclip
 
-for fn in os.listdir("cache"):
+# Populate the cache as required
+for video in sys.argv[1:]:
+	megaclip.get_video_info(video, verbose=True)
+
+for fn in sorted(os.listdir("cache")):
 	info = megaclip.get_video_info(fn.replace(".json", ""), cache_only=True)
 	if info["metadata"]["channel"]["name"] != "devicat": continue
 	print("\t" + info["metadata"]["created_at"] + "\t" + info["metadata"]["_id"])
@@ -16,12 +20,13 @@ for fn in os.listdir("cache"):
 			# Note that CCB's messages don't get counted, but
 			# people talking to her will be. So the spam commands
 			# get counted once, not twice.
+			# msgs += 1
 			continue
 		nextpos = int(msg["content_offset_seconds"])
 		if song is not None and msgs > 0:
-			msg_rate = (nextpos - songpos) / msgs
-			if msgs > 100 or msg_rate < 3:
-				print("%.2f\t%s" % (60/msg_rate, song))
+			msg_rate = 60 * msgs / (nextpos - songpos)
+			if msgs > 100 and msg_rate > 30:
+				print("%.2f\t%d\t%s" % (msg_rate, msgs, song))
 		msgs = 0
 		song = txt[len("Currently playing: "):]
 		songpos = nextpos
