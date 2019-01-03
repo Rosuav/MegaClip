5283bc656961e28373ed871ca462109461fb4281
Chris Angelico <rosuav@gmail.com>
2019-01-03 17:07:35 +1100

    deviquotes: POC out a quote finder

---
+++ deviquotes.py
@@ -0,0 +1,25 @@
+# Search the cache for all quotes from DeviCat and list them
+import os
+import re
+from pprint import pprint
+import megaclip
+quotes = []
+
+def find_quotes(video):
+	info = megaclip.get_video_info(video, cache_only=True)
+	if info["metadata"]["channel"]["name"] not in {"devicat", "devi_cat"}: return
+	print("Scanning video %s..." % video)
+	for msg in info["comments"]:
+		if msg["commenter"]["name"] != "cutiecakebot": continue
+		m = re.match("#([0-9]+): (.*)$", msg["message"]["body"])
+		if not m: continue
+		idx = int(m.group(1))
+		if not idx: raise ValueError("Something wrong in the data for this line: %r" % msg["message"]["body"])
+		quotes.extend([None] * (idx - len(quotes) + 1))
+		quotes[idx] = m.group(2)
+
+for fn in os.listdir("cache"):
+	find_quotes(fn.replace(".json", ""))
+#pprint(quotes)
+for idx in (443, 1, 484, 345):
+	print(idx, quotes[idx])
