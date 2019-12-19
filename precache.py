f7b8da914ce0e0e87d3783e16d96609d15ad12c2
Chris Angelico <rosuav@gmail.com>
2019-12-20 08:37:01 +1100

    precache: Update API calls to use v5 (and Helix for name-to-ID translation)

--- precache.py
+++ precache.py
@@ -7,9 +7,17 @@ import requests # ImportError? "python3 -m pip install requests"
 import keys # ImportError? Look at keys_sample.py and follow the instructions.
 
 def cache_all(channel):
-	url = "https://api.twitch.tv/kraken/channels/%s/videos?broadcast_type=archive&limit=100" % urllib.parse.quote(channel, "")
+	if set(channel) - set("0123456789"):
+		# There are non-digits in the channel name - it's a login, not a user ID.
+		# It's a lot more efficient to look up UIDs first if you have multiple or
+		# are going to do this more than once, but this way, at least it'll work.
+		r = requests.get("https://api.twitch.tv/helix/users", params={"login": channel},
+			headers={"Client-ID": keys.client_id, "Accept": "application/vnd.twitchtv.v5+json"})
+		r.raise_for_status()
+		channel = r.json()["data"][0]["id"]
+	url = "https://api.twitch.tv/kraken/channels/%s/videos?broadcast_type=archive&limit=100" % channel
 	while url:
-		r = requests.get(url, headers={"Authorization": "OAuth " + keys.oauth})
+		r = requests.get(url, headers={"Client-ID": keys.client_id, "Accept": "application/vnd.twitchtv.v5+json"})
 		r.raise_for_status()
 		data = r.json()
 		if not data["videos"]: break # End of list
@@ -21,6 +29,6 @@ def cache_all(channel):
 if __name__ == "__main__":
 	import sys
 	if len(sys.argv) < 2:
-		print("USAGE: python3 cacheall.py channel [channel [channel...]]")
+		print("USAGE: python3 precache.py channel [channel [channel...]]")
 	for channel in sys.argv[1:]:
 		cache_all(channel)
